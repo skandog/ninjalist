ed59cb5835ae5a24d39104d3b03a5816d202f60d
skandog <muumrar@gmail.com>
2022-07-04 21:29:58 +0100

    I forgot to add anything while following along to a tutorial. Dear o dear. I have created 3 basic pages, to demonstrate how the auto routing has happened. I have then createe Navbar and Footer components, with working links, and hardcoded them into just the Home page. Next i will create a Layout component to wrap around the app

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>Copyright 2022 Skanlist</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        THis is a sentence of words that say a thing a few times THis is a
+        sentence of words that say a thing a few times THis is a sentence of
+        words that say a thing a few times{" "}
+      </p>
+      <p>
+        THis is a sentence of words that say a thing a few times THis is a
+        sentence of words that say a thing a few times THis is a sentence of
+        words that say a thing a few times{" "}
+      </p>
+    </div>
+  );
+};
+
+export default About;
